29abf3908ad2328186e27c0503caecc656396f82
Anna Salsano <115489921+annasalsano@users.noreply.github.com>
2022-10-19 12:20:26 +0200

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,20 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+__name__ == '__main__'
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    if n%2 != 0:
+        print("Weird")
+    elif 2 <= n <= 5:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird")
+    elif n >= 20:
+        print("Not Weird")
